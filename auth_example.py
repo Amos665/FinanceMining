5f58e3fbba7ffd13ec6e6af32c1c2a75505b5455
Evan <ewain65@gmail.com>
2018-06-18 23:08:05 -0600

    Add files via upload
    This is the authentication file for the twitter data scraping app

---
+++ auth_example.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 21:08:11 2018
+
+@author: Amos665
+"""
+
+#AUTHENTICATION INFORMATION FOR YOUR TWITTER DATA SCRAPER
+consumer_key = "INSERT YOUR CONSUMER KEY HERE"
+consumer_secret = "INSERT YOUR CONSUMER SECRET HERE"
+access_token = "INSERT YOUR ACCESS TOKEN HERE"
+access_token_secret = "INSERT YOUR ACCESS TOKEN SECRET HERE"
+app_id = "INSERT YOUR APP ID HERE"
